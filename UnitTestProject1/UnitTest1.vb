626949fa058e76156c9e682f9fa36d3147481e2a
KHO11 <54800683+KHO11@users.noreply.github.com>
2024-05-03 11:36:17 +0100

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports weba
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestMethod1()
+        String expectedOutput = fileOperator.ReadStringFromFile("File.csv");
+        String result = systemUnderTest.Parse(somethingtoparse);
+        Assert.Equals(expectedOutput, result);
+        Console.WriteLine("CSV file uploaded")
+
+    End Sub
+
+End Class
