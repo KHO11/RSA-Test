c0100b8084e45f2777f58b240ec8b03071d056f5
KHO11 <54800683+KHO11@users.noreply.github.com>
2024-05-09 09:34:02 +0100

    Add files via upload

--- Controllers/HomeController.vb
+++ Controllers/HomeController.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.IO
 Imports System.Data
+Imports System.Net.Http
+Imports System.Net
+
 Public Class HomeController
     Inherits System.Web.Mvc.Controller
 
@@ -26,30 +29,39 @@ Public Class HomeController
     Function UploadFile(FileUpload1 As FileUpload, GridView1 As GridView) As ActionResult
         ViewData("Message") = "Upload File page."
         Dim newFile As WeatherDetails = New WeatherDetails()
-        Dim csvPath As String = Server.MapPath("~/csvFile/") + Path.GetFileName(FileUpload1.PostedFile.FileName)
-        FileUpload1.SaveAs(csvPath)
 
-        Dim dt As New DataTable()
-        dt.Columns.AddRange(New DataColumn(2) {New DataColumn(newFile.Lat, GetType(Decimal)), New DataColumn(newFile.Longitude, GetType(Decimal)), New DataColumn(newFile.locatioName, GetType(String))})
+        Dim url As String = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+        Dim request As HttpWebRequest = WebRequest.CreateHttp(url)
+        request.Method = "GET"
+        request.ContentType = "application/json"
+        Dim response As HttpWebResponse = CType(request.GetResponse(), HttpWebResponse)
+        If response.StatusCode = HttpStatusCode.OK Then
+            Dim csvPath As String = Server.MapPath("~/csvFile/") + Path.GetFileName(FileUpload1.PostedFile.FileName)
+            FileUpload1.SaveAs(csvPath)
 
-        Dim csvData As String = System.IO.File.ReadAllText(csvPath)
+            Dim dt As New DataTable()
+            dt.Columns.AddRange(New DataColumn(2) {New DataColumn(newFile.Lat, GetType(Decimal)), New DataColumn(newFile.Longitude, GetType(Decimal)), New DataColumn(newFile.locatioName, GetType(String))})
 
-        For Each row As String In csvData.Split(ControlChars.Lf)
-            If Not String.IsNullOrEmpty(row) Then
-                dt.Rows.Add()
-                Dim i As Integer = 0
+            Dim csvData As String = System.IO.File.ReadAllText(csvPath)
 
-                'Execute a loop over the columns.
-                For Each cell As String In row.Split(","c)
-                    dt.Rows(dt.Rows.Count - 1)(i) = cell
-                    i += 1
-                Next
-            End If
-        Next
+            For Each row As String In csvData.Split(ControlChars.Lf)
+                If Not String.IsNullOrEmpty(row) Then
+                    dt.Rows.Add()
+                    Dim i As Integer = 0
+
+                    'Execute a loop over the columns.
+                    For Each cell As String In row.Split(","c)
+                        dt.Rows(dt.Rows.Count - 1)(i) = cell
+                        i += 1
+                    Next
+                End If
+            Next
+            GridView1.DataSource = dt
+            GridView1.DataBind()
+        End If
 
 
-        GridView1.DataSource = dt
-        GridView1.DataBind()
+
         Return View()
     End Function
 
